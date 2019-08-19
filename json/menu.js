e366c0f5ae2721ca9f07b09d793ad7a75ac1c05a
e <1472963799@qq.com>
2019-08-19 18:25:07 +0800

    修改

--- json/menu.js
+++ json/menu.js
@@ -85,54 +85,5 @@
 			"title": "用户权限",
 			"jump": "parameter/jurisdiction"
 		}]
-	}, {
-		"name": "user",
-		"title": "用户",
-		"icon": "layui-icon-user",
-		"list": [{
-			"name": "user",
-			"title": "网站用户",
-			"jump": "user/user/list"
-		}, {
-			"name": "administrators-list",
-			"title": "后台管理员",
-			"jump": "user/administrators/list"
-		}, {
-			"name": "administrators-rule",
-			"title": "角色管理",
-			"jump": "user/administrators/role"
-		}]
-	}, {
-		"name": "set",
-		"title": "设置",
-		"icon": "layui-icon-set",
-		"list": [{
-			"name": "system",
-			"title": "系统设置",
-			"spread": true,
-			"list": [{
-				"name": "website",
-				"title": "网站设置"
-			}, {
-				"name": "email",
-				"title": "邮件服务"
-			}]
-		}, {
-			"name": "user",
-			"title": "我的设置",
-			"spread": true,
-			"list": [{
-				"name": "info",
-				"title": "基本资料"
-			}, {
-				"name": "password",
-				"title": "修改密码"
-			}]
-		}]
-	}, {
-		"name": "get",
-		"title": "授权",
-		"icon": "layui-icon-auz",
-		"jump": "system/get"
 	}]
 }
